f89ff1d7c4ee6425157df1c3a2f28a37f978fca2
David Castillo <49570716+dcastillosaez@users.noreply.github.com>
2019-05-17 23:54:07 +0200

    Add files via upload

--- scripts/environment_variables.sh
+++ scripts/environment_variables.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+REPO=$HOME/BBB
+export REPO
 
-#VARIABLES DE ENTORNO
-export odroid='/home/odroid/'
-export repo_path='/home/odroid/BBB/'
-export documentos='/home/odroid/Documents/'
+SCRIPTS=$REPO/scripts
+export SCRIPTS
 
-#ALIAS
-alias actualiz='apt-get update'
-alias actualizar='sudo apt-get update && sudo apt-get upgrade'
-alias actualizar_repo='cd /home/odroid/BBB/ && git pull'
-alias reiniciar='sudo shutdown -r now'
+GPIOS=/sys/class/gpio
+export GPIOS
+
+
