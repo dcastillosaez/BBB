505e2d87c2fcb7d60c415db04cfd06b2f927c489
David Castillo <49570716+dcastillosaez@users.noreply.github.com>
2019-05-18 00:02:20 +0200

    Update alias_setup.sh

--- scripts/alias_setup.sh
+++ scripts/alias_setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #Configuracion de comandos rapidos
 
-alias bbb_set_permisions='sudo chown -Rf debian $HOME/BBB/* && sudo chgrp -Rfv debian $HOME/BBB/*'
+alias bbb_set_permisions=`sudo chown -Rf debian $HOME/BBB/* && sudo chgrp -Rfv debian $HOME/BBB/*`
 
 #alias bbb_kill_applications ='$SCRIPTS/application_terminate.sh'
 
-alias actualizar_repositorio ='cd $HOME/BBB/ && sudo git pull'
+alias actualizar_repositorio =`cd $HOME/BBB/ && sudo git pull`
 
 alias actualizar='sudo apt-get update -y && sudo apt-get upgrade -y'
 
 #Realiza una copia de seguridad de todo el repositorio en el directorio /temp/backups
-alias copia_seguridad='cd $HOME/BBB/scripts/ && ./security_copy.sh'
+alias copia_seguridad=`cd $HOME/BBB/scripts/ && ./security_copy.sh`
 
 alias limpia_cache='sudo rm -Rf  /home/odroid/.cache/chromium/Default/Cache/* && sudo rm -Rf  /home/odroid/.cache/chromium/Default/Media Cache/*'
 
