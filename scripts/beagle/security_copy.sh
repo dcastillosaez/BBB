1eb0644a0d4cee9b12d243bf50f2261208d503b9
root <root@beaglebone.localdomain>
2019-05-18 18:54:24 +0000

    Modificacion del archivo copia de seguridad

--- scripts/beagle/security_copy.sh
+++ scripts/beagle/security_copy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 #COPIA DE SEGURIDAD DE LOS ARCHIVOS
-PATH=/var/backups/
-#borramos la anterior coia de seguridad
-if [ -f $PATH/Documents.tar.gz ]
-then
-        rm -Rf Documents.tar.gaz
-fi
-#Ahora hacemos la nueva copia de seguridad
-zip /var/backups/Documents.zip /home/debian/
+comando='zip /home/debian/Documents.zip /home/debian/'
+$comando
 
