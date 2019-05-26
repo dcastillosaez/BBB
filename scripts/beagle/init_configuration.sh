e051515a34598b69a2c83a86eae0cc48f98697ed
David Castillo <49570716+dcastillosaez@users.noreply.github.com>
2019-05-26 20:26:09 +0200

    Archivo de inicio de configuracion de la Beagle

---
+++ scripts/beagle/init_configuration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Initial System Configuration 
+#Load Universal_Cape
+#export SLOTS=/sys/devices/platform/bone_capemgr/slots
+#echo BB-PWM0 > $SLOTS
+
+export SLOTS=/sys/devices/platform/bone_capemgr/slots
+echo cape-universaln > $SLOTS
+config-pin P9.22 pwm
