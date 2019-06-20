0ef7f7b77c0a7325badc8ae4fe2da2d6f5cf5b97
dcastillosaez <dacastillosaez@gmail.com>
2019-06-20 18:07:23 +0200

    Ejemplo proyecto servo

--- RobotCarApp/RobotCarApp/main.cpp
+++ RobotCarApp/RobotCarApp/main.cpp
@@ -1,9 +1,39 @@
 #include <QCoreApplication>
 #include "util.h"
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <deque>
+#include <iomanip>
+#include "util.h"
+#include "PWMuniv.h"
+#include <unistd.h>
 
-    return a.exec();
+//Espacios de nombres utilizados
+using namespace BBB;
+using namespace std;
+int main()
+{
+    //Cargamos la configuracion del PWM en concreto
+
+    PWM pwm(PWM::P9_22);
+    pwm.setDutyCycle((unsigned)510000);
+    pwm.setPolarity(PWM::ACTIVE_HIGH);
+    pwm.setPeriod(20000000);  //50kHz (20ms)
+    pwm.setDutyCycle((unsigned)0);
+    pwm.run();
+
+    int min_val=500000;
+    int max_val=2000000;
+    for (int i=min_val; i<max_val; i++) {
+        unsigned int duration=i;
+        pwm.setDutyCycle(duration);
+        usleep(100000);
+        pwm.run();
+    }
+    pwm.stop();
+    return 0;
 }
+
+
 
