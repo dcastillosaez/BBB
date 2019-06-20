17ecffaa78ece7cac05c296d8bfc248d2afa4527
dcastillosaez <dacastillosaez@gmail.com>
2019-06-20 18:33:11 +0200

    Ejemplo proyecto servo

--- RobotCarApp/RobotCarApp/main.cpp
+++ RobotCarApp/RobotCarApp/main.cpp
@@ -25,10 +25,10 @@ int main()
     printf("Inicio de bucle\n");
     int min_val=500000;
     int max_val=2000000;
-    for (int i=min_val; i<max_val; i++) {
+    for (int i=min_val; i<max_val; i+=50) {
         unsigned int duration=i;
         pwm.setDutyCycle(duration);
-        usleep(20000);
+        usleep(50000);
         printf("Iteracion %d\n",i);
         pwm.run();
     }
