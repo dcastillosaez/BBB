41a4c52b17fd3f8b83c218d3bb54b7fccf105488
root <root@arm.localdomain>
2019-06-22 08:26:35 +0000

    Modificacion de parametros del main.cpp

--- RobotCarApp/RobotCarApp/main.cpp
+++ RobotCarApp/RobotCarApp/main.cpp
@@ -15,6 +15,12 @@ using namespace BBB;
 using namespace std;
 int main(int argc, char* argv[])
 {
+    if(argc<2)
+    {
+	printf("Seleccione como argumento el Paso y Velocidad\n");
+	printf("Recomendacion: Paso: 25000   Velocidad: 10000  \n");
+	exit(-1);
+    }
     char *p;
     long step_conv = strtol(argv[1], &p, 10);
     long time_conv = strtol(argv[2], &p, 10);
@@ -26,14 +32,14 @@ int main(int argc, char* argv[])
     printf("Tiempo de delay escogido\n: %d", time);
 
     PWM pwm(PWM::P9_22);
-    pwm.setDutyCycle((unsigned)510000);
+    pwm.setDutyCycle((unsigned)500000);
     pwm.setPolarity(PWM::ACTIVE_HIGH);
     pwm.setPeriod(20000000);  //50kHz (20ms)
     pwm.setDutyCycle((unsigned)0);
     pwm.run();
     printf("Inicio de bucle\n");
     int min_val=500000;
-    int max_val=2000000;
+    int max_val=2650000;
     for (int i=min_val; i<max_val; i+=step) {
         unsigned int duration=i;
         pwm.setDutyCycle(duration);
