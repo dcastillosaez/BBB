aa02c8b726c9006a2683df666178c64fc2a10124
root <root@arm.localdomain>
2019-06-22 09:22:04 +0000

    Añadidos campos de velocidad y step al servo

--- scripts/beagle/init_configuration.sh
+++ scripts/beagle/init_configuration.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#Initial System Configuration 
-#Load Universal_Cape
-#export SLOTS=/sys/devices/platform/bone_capemgr/slots
-#echo BB-PWM0 > $SLOTS
-
 export SLOTS=/sys/devices/platform/bone_capemgr/slots
 echo cape-universaln > $SLOTS
 config-pin -q P9.22
 config-pin P9.22 pwm
+echo "Universal_Cape_Configurado" > /home/debian/log.txt
