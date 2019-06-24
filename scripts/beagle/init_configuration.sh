052d4d3ec403e6eddb2be17be433b08e15df4c14
root <root@arm.localdomain>
2019-06-24 15:34:58 +0000

    Añadido configuracion de GPIOS en init_configuration.sh

--- scripts/beagle/init_configuration.sh
+++ scripts/beagle/init_configuration.sh
@@ -3,4 +3,10 @@ export SLOTS=/sys/devices/platform/bone_capemgr/slots
 echo cape-universaln > $SLOTS
 config-pin -q P9.22
 config-pin P9.22 pwm
-echo "Universal_Cape_Configurado" > /home/debian/log.txt
+#Configuracion de Pines que actuaran como GPIOS
+config-pin P8.7 gpio
+config-pin -q P8.7 
+config-pin P8.8 gpio
+config-pin -q P8.8
+
+
