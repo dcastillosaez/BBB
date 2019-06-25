7db75693e74c17f10bb7816977bf9f4b9720aa95
David Castillo <49570716+dcastillosaez@users.noreply.github.com>
2019-06-25 18:43:48 +0200

    Añadidos GPIOs y PWM

--- scripts/beagle/init_configuration.sh
+++ scripts/beagle/init_configuration.sh
@@ -3,10 +3,16 @@ export SLOTS=/sys/devices/platform/bone_capemgr/slots
 echo cape-universaln > $SLOTS
 config-pin -q P9.22
 config-pin P9.22 pwm
+config-pin -q P9.14
+config-pin P9.14 pwm
 #Configuracion de Pines que actuaran como GPIOS
 config-pin P8.7 gpio
 config-pin -q P8.7 
 config-pin P8.8 gpio
 config-pin -q P8.8
+config-pin P8.9 gpio
+config-pin -q P8.9
+config-pin P8.10 gpio
+config-pin -q P8.10
 
 
